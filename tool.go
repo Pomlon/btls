cf20f9c814a4833143e073f3f4fb531a5caec87c
Marcin Janeczek <mjaneczek1990@gmail.com>
2017-10-30 21:25:45 +0100

    moved everything but main to other files, commented main code for now since it's basically test

--- tool.go
+++ tool.go
@@ -1,49 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"os/exec"
-)
-
-type Proc struct {
-	ChanIn  chan string
-	Running bool
-}
-
-func (p *Proc) ExecCommand(command string) {
-	p.ChanIn = make(chan string, 100)
-	go p.Command(command, p.ChanIn)
-}
-
-func (p *Proc) Command(command string, cha <-chan string) {
-	cmd := exec.Command(command)
-
-	stdout, _ := cmd.StdoutPipe()
-	defer stdout.Close()
-	cmd.Start()
-	p.Running = true
-	for p.Running == true {
-		fmt.Println("SELECTIN")
-		select {
-		case msgIn := <-cha:
-			if msgIn == "kill" {
-				cmd.Process.Kill()
-				p.Running = false
-			}
-
-		default:
-			fmt.Println("Executing default")
-			buf := make([]byte, 2000)
-			stdout.Read(buf)
-			fmt.Println(string(buf))
-		}
-	}
-}
-
 func main() {
-	proc := Proc{}
+	/*proc := Proc{}
 	fmt.Println("Executing")
 	proc.ExecCommand("./courierApp.exe")
 
@@ -60,5 +18,5 @@ func main() {
 		if reader.Text() == "out" {
 			os.Exit(0)
 		}
-	}
+	}*/
 }
