df771f13a6b912b0f06ea4e3fc88874abc5aad92
Marcin Janeczek <mjaneczek1990@gmail.com>
2017-10-30 20:36:53 +0100

    intial process spawning and folder watching

---
+++ watcher/watch.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/radovskyb/watcher"
+)
+
+func main() {
+	w := watcher.New()
+
+	// SetMaxEvents to 1 to allow at most 1 event's to be received
+	// on the Event channel per watching cycle.
+	//
+	// If SetMaxEvents is not set, the default is to send all events.
+	w.SetMaxEvents(1)
+
+	w.IgnoreHiddenFiles(true)
+	w.Ignore("D:\\Go\\src\\github.com\\Pomlon\\courierApp\\static")
+
+	go func() {
+		for {
+			select {
+			case event := <-w.Event:
+				fmt.Println(event) // Print the event's info.
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	// Watch this folder for changes.
+	if err := w.Add("."); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Watch test_folder recursively for changes.
+	if err := w.AddRecursive("D:\\Go\\src\\github.com\\Pomlon\\courierApp\\"); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print a list of all of the files and folders currently
+	// being watched and their paths.
+	for path, f := range w.WatchedFiles() {
+		fmt.Printf("%s: %s\n", path, f.Name())
+	}
+
+	fmt.Println()
+
+	// Start the watching process - it'll check for changes every 100ms.
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		log.Fatalln(err)
+	}
+}
