5eceda9ad6769555e8094638bf033d528fa44e4d
Marcin Janeczek <mjaneczek1990@gmail.com>
2017-10-31 21:35:32 +0100

    dropped sophisticated goroutines, now simply starts process and redirects stdout and stderr. Waits on murder.

--- proc.go
+++ proc.go
@@ -2,63 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
-	"time"
 )
 
 type Proc struct {
-	ChanIn  chan string
-	Running bool
+	ChanIn chan string
+	proc   *exec.Cmd
 }
 
 func (p *Proc) ExecCommand(command string) {
-	p.ChanIn = make(chan string, 100)
-	go p.Command(command, p.ChanIn)
-}
+	p.proc = exec.Command(command)
 
-func (p *Proc) Command(command string, cha <-chan string) {
-	cmd := exec.Command(command)
+	p.proc.Stdout = os.Stdout
+	p.proc.Stderr = os.Stderr
 
-	err := cmd.Start()
+	err := p.proc.Start()
 	if err != nil {
 		panic(err)
 	}
 
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer stdout.Close()
-
-	time.Sleep(time.Millisecond * 1000)
-	p.Running = true
-	for p.Running == true {
-		//fmt.Println("SELECTIN")
-		select {
-		case msgIn := <-cha:
-			if msgIn == "kill" {
-				stdout.Close()
-				cmd.Process.Kill()
-				p.Running = false
-			}
-
-		default:
-
-			//fmt.Println("Executing default")
-			buf := make([]byte, 2000)
-			n, _ := stdout.Read(buf)
-			if str := string(buf); n > 0 {
-				fmt.Println(str)
-			}
-
-		}
-	}
 }
 
 //Murder murders the running process
 func (p *Proc) Murder() {
-	p.ChanIn <- "kill"
+	p.proc.Process.Kill()
+	p.proc.Process.Wait()
 }
 
 //OneRun runs command once, waits for it and returns output.
